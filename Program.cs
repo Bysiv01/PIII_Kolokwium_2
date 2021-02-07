1d5145b22d76b47f74f8297c8fc10859700909b6
Bysiv01 <45607527+Bysiv01@users.noreply.github.com>
2021-02-07 09:38:52 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PIII_Kolokwium_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
